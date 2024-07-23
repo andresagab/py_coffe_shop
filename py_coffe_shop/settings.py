b7d73a5184dca73ae5b70985c5c88cc702831ca4
Andrés Angulo <andrescabj981@gmail.com>
2024-07-22 23:54:21 -0500

    Setup postgresql as db of project, also add psycopg2-binary as dev requirement

--- py_coffe_shop/settings.py
+++ py_coffe_shop/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'py_coffe_shop.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev_py_coffe_shop',
+        'USER': 'postgres',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
