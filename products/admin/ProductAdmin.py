5cc371e38c0ea23cb8e81b3e204284657f517ab0
Andrés Angulo <andrescabj981@gmail.com>
2024-08-04 22:29:48 -0500

    v0.14.0 Add 'created_at' column to Product model
    Create migration and update the info_product.html and list_product.html templates to enable the 'new' label with validation, add function in Product model, finally update ProductAdmin to add the new field to list_display

--- products/admin/ProductAdmin.py
+++ products/admin/ProductAdmin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from products.models import Product
 
+
 class ProductAdmin(admin.ModelAdmin):
     model = Product
-    list_display = ['name', 'price', 'available']
+    list_display = ['name', 'price', 'available', 'created_at']
     search_fields = ['name']
+
 
 admin.site.register(Product, ProductAdmin)
--- products/models/Product.py
+++ products/models/Product.py
@@ -1,3 +1,5 @@
+from datetime import timedelta, date
+
 from django.db import models
 
 
@@ -7,9 +9,18 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="price")
     available = models.BooleanField(default=True, verbose_name="available")
     photo = models.ImageField(upload_to="media/logos", null=True, blank=True, verbose_name="photo")
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
 
     def __str__(self) -> str:
         return self.name
 
     def get_avg_rating(self):
         return self.productrating_set.aggregate(models.Avg("rating")).get("rating__avg")
+
+    def still_new(self):
+        still = False
+        # if model has created_at, and it is less than 60 days
+        if self.created_at and (self.created_at.date() + timedelta(days=60)) >= date.today():
+            still = True
+
+        return still
