e89886f5d5c1443f78d4dc3460c9e819344d6a33
Andrés Angulo <andrescabj981@gmail.com>
2024-08-05 23:35:38 -0500

    v0.15.1 Update ProductListView to not load inactive products

--- products/views/ProductListView.py
+++ products/views/ProductListView.py
@@ -3,8 +3,13 @@ from django.views.generic import ListView
 
 from products.models import Product
 
-class ProductListView(ListView):
 
+class ProductListView(ListView):
     model = Product
     template_name = "products/list_product.html"
     context_object_name = "products"
+
+    def get_context_data(self, **kwargs: Any) -> Any:
+        object_list = Product.objects.filter(available=True)
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        return context
